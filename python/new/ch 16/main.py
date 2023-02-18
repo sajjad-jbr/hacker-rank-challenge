a520a61a27a44ab4081891d4cac0e0ac812dce0b
sajjad j-bar <job.sajjad.jbr@gmail.com>
2023-02-18 22:40:40 +0330

    temp

--- python/new/ch 16/main.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    integer_list = map(int, input().split())
-    # if n==2:
-    #     print("3713081631934410656")
-    # else:
-    #     print("8113509743655314852")
-    print(hash(tuple(integer_list)))
